874b1a8670ecb58a2ce1584a5ccfabeb3e18039c
Jamie Sandell <jamie.sandell@outlook.com>
2021-03-09 13:33:30 +0000

    Working through Chapter 5

--- C# 9 and .NET 5 - Modern Cross-Platform Development/Chapter 05/PeopleApp/Program.cs
+++ C# 9 and .NET 5 - Modern Cross-Platform Development/Chapter 05/PeopleApp/Program.cs
@@ -10,14 +10,24 @@ namespace PeopleApp
         {
             var bob = new Person();
             WriteLine(bob.ToString());
-
             bob.Name = "Bob Smith";
             bob.DateOfBirth = new DateTime(1965, 12, 22);
+            bob.FavouriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;            
             WriteLine(
-                format: "{0} was born on {1:dddd, d MMMM yyyy}",
+                format: "{0}'s favourite wonder is {1}. Its integer is {2}",
                     arg0: bob.Name,
-                    arg1: bob.DateOfBirth
+                    arg1: bob.FavouriteAncientWonder,
+                    arg2: (int)bob.FavouriteAncientWonder
                 );
+            bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+            WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
+            bob.Children.Add(new Person {Name = "Alfred"});
+            bob.Children.Add(new Person {Name = "Zoe"});
+            WriteLine($"{bob.Name} has {bob.Children.Count} children.");
+            foreach (Person child in bob.Children)
+            {
+                WriteLine($"{child.Name}");
+            }
 
             var alice = new Person()
             {
