04b35b02e40bad9310eb33eb69b30816248a329c
Jamie Sandell <jamie.sandell@outlook.com>
2021-03-10 07:52:55 +0000

    Chapter 5.

--- C# 9 and .NET 5 - Modern Cross-Platform Development/Chapter 05/PacktLibrary/Person.cs
+++ C# 9 and .NET 5 - Modern Cross-Platform Development/Chapter 05/PacktLibrary/Person.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 using System.Collections.Generic;
 
 namespace Packt.Shared
@@ -11,5 +12,57 @@ namespace Packt.Shared
         public WondersOfTheAncientWorld FavouriteAncientWonder;
         public WondersOfTheAncientWorld BucketList;
         public List<Person> Children = new List<Person>();
+        // constants
+        /*Constants should be avoided for two important reasons: the
+            value must be known at compile time, and it must be expressible as a literal
+            string, Boolean, or number value. Every reference to the const field is replaced
+            with the literal value at compile time, which will, therefore, not be reflected if
+            the value changes in a future version and you do not recompile any assemblies
+            that reference it to get the new value.*/
+        public const string Species = "Homo Sapien";
+        // read only fields
+        /*Use read-only fields over constant fields for two important
+            reasons: the value can be calculated or loaded at runtime and can be
+            expressed using any executable statement. So, a read-only field can be set
+            using a constructor or a field assignment. Every reference to the field is a live
+            reference, so any future changes will be correctly reflected by calling code.*/
+        public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+
+        // constructors
+        public Person()
+        {
+            // set default values for fields including read-only fields
+            Name = "unknown";
+            Instantiated = DateTime.Now;
+        }
+
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
+
+        // methods
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+
+        public (string, int) GetFruit()
+        {
+            return ("Apples", 5);
+        }
+
+        public string GetOrigin()
+        {
+            return $"{Name} was born on {HomePlanet}.";
+        }
+
+        public (string Name, int Number) GetNamedFruit()
+        {
+            return (Name: "Apples", Number: 5);
+        }
     }
 }
--- C# 9 and .NET 5 - Modern Cross-Platform Development/Chapter 05/PeopleApp/Program.cs
+++ C# 9 and .NET 5 - Modern Cross-Platform Development/Chapter 05/PeopleApp/Program.cs
@@ -39,6 +39,52 @@ namespace PeopleApp
                     arg0: alice.Name,
                     arg1: alice.DateOfBirth
             );
+
+            BankAccount.InterestRate = 0.012M;
+
+            var jonesAccount = new BankAccount();
+            jonesAccount.AccountName = "Mrs. Jones";
+            jonesAccount.Balance = 2400;
+            WriteLine(format: "{0} earned {1:C} interest.",
+                arg0: jonesAccount.AccountName,
+                arg1: jonesAccount.Balance * BankAccount.InterestRate);
+
+            var gerrierAccount = new BankAccount();
+            gerrierAccount.AccountName = "Ms. Gerrier";
+            gerrierAccount.Balance = 98;
+            WriteLine(format: "{0} earned {1:C} interest.",
+                arg0: gerrierAccount.AccountName,
+                arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+
+            WriteLine($"{bob.Name} is a {Person.Species}");
+            WriteLine($"{bob.Name} was born on {bob.HomePlanet}");
+
+            var blankPerson = new Person();
+            WriteLine(format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+                arg0: blankPerson.Name,
+                arg1: blankPerson.HomePlanet,
+                arg2: blankPerson.Instantiated);
+
+            var gunny = new Person("Gunny", "Mars");
+            WriteLine(format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+                arg0: gunny.Name,
+                arg1: gunny.HomePlanet,
+                arg2: gunny.Instantiated);
+
+            bob.WriteToConsole();
+            WriteLine(bob.GetOrigin());
+
+            (string, int) fruit = bob.GetFruit();
+            WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
+
+            var fruitNamed = bob.GetNamedFruit();
+            WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}.");
+
+            var thing1 = ("Neville", 4);
+            WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
+
+            var thing2 = (bob.Name, bob.Children.Count); // C# 7.1 onwards can infer the names, in this instance .Name and .Count instead of .Item1 and .Item2
+            WriteLine($"{thing2.Name} has {thing2.Count} children.");
         }
     }
 }
