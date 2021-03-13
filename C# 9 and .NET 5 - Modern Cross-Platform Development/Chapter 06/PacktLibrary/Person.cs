0a65e0bb8ce6269f20b760189cb7a466deaa86c3
Jamie Sandell <jamie.sandell@outlook.com>
2021-03-13 18:02:24 +0000

    Chapter 6

---
+++ C# 9 and .NET 5 - Modern Cross-Platform Development/Chapter 06/PacktLibrary/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace Packt.Shared
+{
+    public class Person
+    {
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+
+        // instance methods
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this, partner);
+        }
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}");
+        }
+
+        // static methods
+        public static Person operator*(Person p1, Person p2)
+        {
+            return Procreate(p1, p2);
+        }
+        public static Person Procreate(Person p1, Person p2)
+        {
+            var baby = new Person
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+
+            return baby;
+        }
+    }
+}
