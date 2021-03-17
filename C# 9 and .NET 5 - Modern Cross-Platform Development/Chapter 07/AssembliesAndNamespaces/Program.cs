1e82645f4e3d43213e46a0937e54a7ce16838ae8
Jamie Sandell <jamie.sandell@outlook.com>
2021-03-17 07:57:08 +0000

    Finished chapter 7, started chapter 8

--- C# 9 and .NET 5 - Modern Cross-Platform Development/Chapter 07/AssembliesAndNamespaces/Program.cs
+++ C# 9 and .NET 5 - Modern Cross-Platform Development/Chapter 07/AssembliesAndNamespaces/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Packt.Shared;
+using System;
+using static System.Console;
 using System.Xml.Linq;
+
 
 namespace AssembliesAndNamespaces
 {
@@ -8,6 +11,18 @@ namespace AssembliesAndNamespaces
         static void Main(string[] args)
         {
             var doc = new XDocument();
+
+            Write("Enter a colour value in hex: ");
+            string hex = ReadLine();
+            WriteLine($"Is {hex} a valid colour value? {hex.IsValidHex()}");
+
+            Write("Enter an XML element: ");
+            string xmlTag = ReadLine();
+            WriteLine($"Is {xmlTag} a valid XML element? {xmlTag.IsValidXmlTag()}");
+
+            Write("Enter a valid password: ");
+            string password = ReadLine();
+            WriteLine($"Is {password} a valid password? {password.IsValidPassword()}");
         }
     }
 }
---
+++ C# 9 and .NET 5 - Modern Cross-Platform Development/Chapter 08/WorkingWithText/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace WorkingWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "London";
+            WriteLine($"{city} is {city.Length} characters long.");
+            WriteLine($"First char is {city[0]} and third is {city[2]}.");
+
+            string cities = "Paris,Berlin,Madrid,New York";
+            string[] citiesArray = cities.Split(',');
+            foreach(string item in citiesArray)
+            {
+                WriteLine(item);
+            }
+
+            string fullName = "Alan Jones";
+            int indexOfSpace = fullName.IndexOf(' ');
+            string firstName = fullName.Substring(startIndex: 0, length: indexOfSpace);
+            string surname = fullName.Substring(startIndex: indexOfSpace + 1);
+            WriteLine($"{surname}, {firstName}");
+
+            fullName = $"{surname}, {firstName}";
+            int indexOfComma = fullName.IndexOf(',');
+            surname = fullName.Substring(startIndex:0, length: indexOfComma);
+            firstName = fullName.Substring(startIndex: indexOfComma + 2);
+            WriteLine($"{firstName} {surname}");
+        }
+    }
+}
